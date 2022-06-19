22c99fb8dcd67cdccd699a8885934e3a9becc5b8
MartaGreen <iguana13131@gmail.com>
2022-06-19 17:45:28 +0400

    refactor: change structure of offer generation functions

--- kote/src/components/cardPreview/cardPreview.tsx
+++ kote/src/components/cardPreview/cardPreview.tsx
@@ -16,9 +16,36 @@ function CardPreview({
   const getLastDigit = (num: number) => {
     return Number(num.toString().slice(-1));
   };
+  const getTwoLastDigit = (num: number) => {
+    return Number(num.toString().slice(-2));
+  };
 
-  const transfromServingsMsg = (amount: number) => {
-    const servingStr = (servingAmount: number, servingWord: string) => {
+  const createJSXWithRightWordForm = (
+    wordForms: { [num: number]: string },
+    amount: number,
+    jsxGeneratorFunc: Function
+  ) => {
+    // when amount number ends with 11 - 19 or 0 or 5 - 9 word form is the same. The key is 0
+    // when amount number ends with 1 the key is 1
+    // when amount number ends with 2 - 4 the key is 2
+    // So the wordFroms obj has three keys: 0, 1, 2
+    if (
+      (getTwoLastDigit(amount) >= 11 && getTwoLastDigit(amount) <= 19) ||
+      getLastDigit(amount) === 0 ||
+      (getLastDigit(amount) >= 5 && getLastDigit(amount) <= 9)
+    ) {
+      return jsxGeneratorFunc(amount, wordForms["0"]);
+    }
+    if (getLastDigit(amount) === 1) {
+      return jsxGeneratorFunc(amount, wordForms["1"]);
+    }
+    if (getLastDigit(amount) >= 2 && getLastDigit(amount) <= 4) {
+      return jsxGeneratorFunc(amount, wordForms["2"]);
+    }
+  };
+
+  const servingsOfferJSX = (amount: number) => {
+    const offerJSX = (servingAmount: number, servingWord: string) => {
       return (
         <p
           className={classes.description__offer}
@@ -29,39 +56,21 @@ function CardPreview({
       );
     };
 
-    if (
-      (amount >= 11 && amount <= 19) ||
-      getLastDigit(amount) === 0 ||
-      (getLastDigit(amount) >= 5 && getLastDigit(amount) <= 9)
-    ) {
-      return servingStr(amount, "порций");
-    }
-    if (getLastDigit(amount) === 1) {
-      return servingStr(amount, "порция");
-    }
-    if (getLastDigit(amount) >= 2 && getLastDigit(amount) <= 4) {
-      return servingStr(amount, "порции");
-    }
+    const wordForms = { 0: "порций", 1: "порция", 2: "порции" };
+    return createJSXWithRightWordForm(wordForms, amount, offerJSX);
   };
 
-  const mouseGift = (servingsAmount: number) => {
+  const mouseGiftOfferJSX = (servingsAmount: number) => {
     const mouseAmount = Math.ceil(servingsAmount / 20);
-    const mouseOfferStr = (mouseAmount: number, mouseWord: string) => (
+    const offerJSX = (mouseAmount: number, mouseWord: string) => (
       <p className={classes.description__offer} style={{ letterSpacing: 0.1 }}>
         {" "}
         <b>{mouseAmount === 1 ? "" : mouseAmount}</b> {mouseWord} в подарок{" "}
       </p>
     );
 
-    if (getLastDigit(mouseAmount) === 1)
-      return mouseOfferStr(mouseAmount, "мышь");
-    if (
-      (getLastDigit(mouseAmount) >= 11 && getLastDigit(mouseAmount) <= 19) ||
-      (getLastDigit(mouseAmount) >= 5 && getLastDigit(mouseAmount) <= 9)
-    )
-      return mouseOfferStr(mouseAmount, "мышей");
-    if (getLastDigit(mouseAmount) >= 2 && getLastDigit(mouseAmount) <= 4)
-      return mouseOfferStr(mouseAmount, "мыши");
+    const wordForms = { 0: "мышей", 1: "мышь", 2: "мыши" };
+    return createJSXWithRightWordForm(wordForms, mouseAmount, offerJSX);
   };
 
   const totalWeight = ((WEIGHT_PER_SERVING * data.servingsAmount) / 1000)
@@ -76,8 +85,8 @@ function CardPreview({
           <p className={classes.description__dish}>Сказочное заморское яство</p>
           <h2 className={classes.description__title}>Нямушка</h2>
           <h3 className={classes.description__taste}>{data.taste}</h3>
-          {transfromServingsMsg(data.servingsAmount)}
-          {mouseGift(data.servingsAmount)}
+          {servingsOfferJSX(data.servingsAmount)}
+          {mouseGiftOfferJSX(data.servingsAmount)}
         </div>
         <div className={classes.description__weight}>
           <p style={{ fontSize: 42, lineHeight: "38px" }}>{totalWeight}</p>
